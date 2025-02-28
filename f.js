1fe48c04aafc932fcbf929a29583e33403754537
first-- javascript <7225vishnumahor@gmail.com>
2025-02-28 11:44:06 +0530

    add files

---
+++ f.js
@@ -0,0 +1,8 @@
+function run(){
+    let img1=document.querySelector('.div1');
+    img1.style.backgroundImage="url(https://www.portotheme.com/wordpress/porto/shop46/wp-content/uploads/sites/225/2023/09/porto46-category-2.jpg)"
+}
+function run1(){
+    let img1=document.querySelector('.div1');
+    img1.style.backgroundImage="url(https://www.portotheme.com/wordpress/porto/shop46/wp-content/uploads/sites/225/2023/09/porto46-category-1.jpg)"
+}
---
+++ t.js
@@ -0,0 +1,20 @@
+function run(){
+    let img1=document.querySelector('.img1');
+    img1.style.scale="1.2";
+    img1.style.transition="scale 1s"
+}
+function run1(){
+    let img1=document.querySelector('.img1');
+    img1.style.scale="1";
+    img1.style.transition="scale 1s"
+}
+function evg(){
+    let img2=document.querySelector('.img2');
+    img2.style.scale="1.2";
+    img2.style.transition="scale 1s"
+}
+function evg1(){
+    let img2=document.querySelector('.img2');
+    img2.style.scale="1";
+    img2.style.transition="scale 1s"
+}

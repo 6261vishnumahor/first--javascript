391d446ad241e95748b4a4412a7dc88354361470
first-- javascript <7225vishnumahor@gmail.com>
2025-03-03 11:34:45 +0530

    add files

---
+++ photo.js
@@ -0,0 +1,25 @@
+function run1(){
+    let img1 =document.querySelector('.img7');
+    img1.src="https://www.thewowstyle.com/wp-content/uploads/2015/01/images-of-nature-4.jpg"
+}
+function run(){
+    let img1 =document.querySelector('.img7');
+    img1.src="https://th.bing.com/th/id/OIP.U_VJuupQohwnzXcKMztqWgHaEo?w=236&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+}
+function run2(){
+    let img1 =document.querySelector('.img7');
+    img1.src="https://th.bing.com/th/id/OIP.U_VJuupQohwnzXcKMztqWgHaEo?rs=1&pid=ImgDetMain"
+}
+function run3(){
+    let img1 =document.querySelector('.img7');
+    img1.src="https://i.pinimg.com/originals/e5/9b/8b/e59b8bf4007df9103b891051875cee9c.jpg"
+}
+function run4(){
+    let img1 =document.querySelector('.img7');
+    img1.src="https://thumbs.dreamstime.com/z/big-data-background-abstract-digital-technology-flow-pattern-d-particles-copy-space-generative-ai-illustration-317593616.jpg"
+}
+function run5(){
+    let img1 =document.querySelector('.img7');
+    img1.src="https://wallpaperaccess.com/full/4723253.jpg"
+}
+

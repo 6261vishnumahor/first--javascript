636b2ea8b484a9418ac745b0cf2eb3b0f24f58f4
6261vishnumho <7225vishnumahor@gmail.com>
2025-04-05 14:00:25 +0530

    add files

---
+++ array.js/array-filter.js
@@ -0,0 +1,3 @@
+let a=[1,2,34,54,67,87,5,3,5,7,8]
+    b = a.filter(e=>e<10)
+console.log(b)
---
+++ array.js/array-map3.js
@@ -0,0 +1,100 @@
+let card = [
+    {
+        "id": 1,
+        "price":5456,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 2,
+        "price":2356,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 3,
+        "price":3245,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 5,
+        "price":1457,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 6,
+        "price":3456,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 7,
+        "price":3236,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 8,
+        "price":2346,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 9,
+        "price":5456,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 10,
+        "price":5674,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 11,
+        "price":4556,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 12,
+        "price":3896,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    },
+    {
+        "id": 13,
+        "price":4829,
+        "name": "laotop",
+        "brand": "asur",
+        "image": "https://th.bing.com/th/id/R.a243c72be94e93f1399f3399b06c7677?rik=hrhQ9%2b%2fJ1SSPHA&riu=http%3a%2f%2fwww.riskmanagementmonitor.com%2fwp-content%2fuploads%2f2014%2f12%2fLaptop1.jpg&ehk=OfidPRNnM1a1JERcjUs9J725LwV1tT7YdUTEmeAi5Gw%3d&risl=1&pid=ImgRaw&r=0"
+    }
+]
+let t=card.filter(e=>e.price>3000)
+console.log(t)
+
+// let b = card
+let f = document.querySelector('#output')
+f.innerHTML = t.map(card => `
+    <div>
+    <div><img width="100px" src="${card.image}"></div>
+    <div id="h">${card.id}</div>
+     <div >${card.price}</div>
+    <div id="h1">${card.name}</div>
+    <div id="h3">${card.brand}</div>
+    </div>
+    `).join(" ")

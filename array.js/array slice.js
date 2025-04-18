d5935c07dc5281e6e8f544bb7955c235fa8dc7df
6261vishnumho <7225vishnumahor@gmail.com>
2025-04-18 12:34:26 +0530

    files

---
+++ array.js/array slice.js
@@ -0,0 +1,31 @@
+let arr=[2,43,4,54,65,76,86,9,56]
+let arr1=['ghhjs','hdswh','arfre','effd']
+
+// console.log(arr.length)
+// console.log(arr.slice(2,5))
+// console.log(arr.slice(2,arr.length))
+// arr.splice(2,1)
+// console.log(arr)
+// arr.splice(4,0,45,56,67,67)
+// console.log(arr)
+// arr.splice(arr.length-1,1)
+// console.log(arr)
+
+// arr.splice(arr.length,0,34,45,6,7,4)
+
+// console.log(arr)
+// arr.splice(0,0,3435,5,35,643,345,)
+// console.log(arr)
+// arr.sort((a,b)=>a-b)
+// console.log(arr)
+// arr1.sort()
+// console.log(arr1)
+// arr.reverse()
+//     console.log(arr)
+// a=arr.reduce((a,b)=>a+b)
+// console.log(a)
+// console.log(arr.reduce((a,b)=>a+b))
+arr3=arr.concat(arr1)
+console.log(arr3)
+arr4=[...arr,...arr1]
+console.log(arr4)
